aa6a79c6b84e2cf94929d680ce6785a49d8b57ca
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2021-09-18 18:43:07 +0300

    feat: implement importProductsFile

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -1,31 +1,50 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
+import importProductsFile from '@functions/import-product';
 
 const serverlessConfiguration: AWS = {
   service: 'import-service',
   frameworkVersion: '2',
+  useDotenv: true,
   custom: {
     webpack: {
       webpackConfig: './webpack.config.js',
-      includeModules: true,
+      includeModules: true
     },
+    bucketName: "tamiko-shop-csv-upload",
+    uploadFolderName: "uploaded"
   },
-  plugins: ['serverless-webpack'],
+  plugins: [
+    "serverless-webpack",
+    "serverless-offline",
+  ],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    stage: "dev",
+    region: "eu-west-1",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      S3_BUCKET_NAME: "${self:custom.bucketName}"
     },
+    iamRoleStatements: [
+      {
+        'Effect': 'Allow',
+        'Action': ['s3:*'],
+        'Resource': [
+          "arn:aws:s3:::${self:custom.bucketName}",
+          "arn:aws:s3:::${self:custom.bucketName}/*",
+          "arn:aws:s3:::${self:custom.bucketName}/*/*"
+        ]
+      },
+    ],
     lambdaHashingVersion: '20201221',
   },
-  // import the function via paths
-  functions: { hello },
+  functions: { importProductsFile },
 };
 
 module.exports = serverlessConfiguration;
