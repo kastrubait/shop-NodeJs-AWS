443eb51df49bba2eaf67c0fff12ddbc7d3fd799a
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2021-10-07 22:01:15 +0300

    refactor: importFileService

--- import-service/src/functions/service/import-file-servise.ts
+++ import-service/src/functions/service/import-file-servise.ts
@@ -4,22 +4,53 @@ import {
     GetObjectCommand,
     S3Client,
   } from "@aws-sdk/client-s3";
-  import csvParser from "csv-parser";
-  import { Readable } from "stream";
+import { S3EventRecord } from "aws-lambda";
+import { validateSync, ValidationError } from "class-validator";
+import csvParser from "csv-parser";
+import { ProductDto } from "src/dto/product.dto";
+import { Readable } from "stream";
+import queueService from "./queue.service";
   
   const BUCKET_NAME = "tamiko-shop-csv-upload";
   const s3Client = new S3Client({
     region: "eu-west-1",
   });
   
-  export const handleImportedFiles = async (records): Promise<any> => {
+  export const handleImportedFiles = async (records: S3EventRecord[]): Promise<any> => {
+    const promiseProducts = records.map((record) => {
+      return importedFile(record);
+    });
+    const arrProducts = await Promise.all(promiseProducts);
+    const results = arrProducts.flat();
+    console.log("handleImportedFiles results", results);
+    return results;
+  };
+  
+  const importedFile = async (record: S3EventRecord) => {
     try {
-      for (const record of records) {
-        await saveProducts(record);
+      const keyObject = record.s3.object.key;
+      const getObjectParams = {
+        Bucket: BUCKET_NAME,
+        Key: keyObject,
+      };
+      const command = new GetObjectCommand(getObjectParams);
+      const item = await s3Client.send(command);
+      const products = await readStream(item.Body);
+      console.log(`saved products`, products);
+      const validationErrors: ValidationError[][] | null =
+      validateProducts(products);
+
+      if (validationErrors) {
+        console.error(`Validation failed for ${keyObject}`, validationErrors)
+      } else {
         await moveFileToParsedFolder(record);
+        products.forEach(async (product: ProductDto) => {
+          await queueService.addToQueue(product);
+        })
+        return products;
       }
     } catch (error) {
-      console.log("handleImportedFiles Error", error);
+      console.error("Process importedFile Error", error);
       throw error;
     }
   };
@@ -28,23 +59,7 @@ import {
     await copyFileToParsedFolder(record);
     await deleteFromUploaded(record);
   };
-  
-  const saveProducts = async (record) => {
-    try {
-      const getObjectParams = {
-        Bucket: BUCKET_NAME,
-        Key: record.s3.object.key,
-      };
-      const command = new GetObjectCommand(getObjectParams);
-      const item = await s3Client.send(command);
-      const products = await readStream(item.Body);
-      console.log(`saved products`, products);
-    } catch (error) {
-      console.log("saveProducts Error", error);
-      throw error;
-    }
-  };
-  
+
   const readStream = (stream: Readable): Promise<any> => {
     const allRowsData = [];
     return new Promise((resolve, reject) => {
@@ -63,6 +78,18 @@ import {
     });
   };
   
+  export const validateProducts = (data: ProductDto[]): ValidationError[][] | null => {
+    let hasErrors = false;
+    const validationErrors = data.map((data) => {
+      const dtoInstance: ProductDto = new ProductDto(data);
+      console.log("dtoInstance ", dtoInstance);
+      const errors: ValidationError[] = validateSync(dtoInstance);
+      if (errors.length) hasErrors = true;
+      return errors;
+    });
+    return hasErrors ? validationErrors : null;
+  };
+
   export const copyFileToParsedFolder = async (record) => {
     const commandParameters = {
       Bucket: record.s3.bucket.name,
