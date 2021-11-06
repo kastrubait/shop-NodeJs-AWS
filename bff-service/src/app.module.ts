cde6f9d97a08681df0dfa3249a71d3d59a5b4af8
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2021-11-06 23:30:00 +0300

    feat: add caching

--- bff-service/src/app.module.ts
+++ bff-service/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
@@ -6,7 +6,11 @@ import { HttpModule } from "@nestjs/axios";
 import { RecipientInterceptor } from './recipient.interceptor';
 
 @Module({
-  imports: [HttpModule, ConfigModule.forRoot()],
+  imports: [
+    HttpModule, 
+    ConfigModule.forRoot(),
+    CacheModule.register(),
+  ],
   controllers: [AppController],
   providers: [AppService, RecipientInterceptor],
 })
